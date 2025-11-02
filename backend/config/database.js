42e454823855f4eb55dc67b761c1773ce3a15911
ReStart-Academy <restart18112024@gmail.com>
2025-11-02 18:50:34 +0530

    database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,20 @@
+// config/database.js
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // 👈 this line is critical for Render
+    },
+  },
+  logging: false,
+});
 
 export default sequelize;
+
